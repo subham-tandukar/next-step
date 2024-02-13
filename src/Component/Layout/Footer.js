019d60f0439dec58cd7e5c2d08186cd21cf202fc
Subham <subhamtndkr9@gmail.com>
2024-02-13 21:29:22 +0545

    push

--- src/Component/Layout/Footer.js
+++ src/Component/Layout/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="site-footer">
-      <span>&copy; 2023 . All rights reserved</span>
+      <span>&copy; 2024 . All rights reserved</span>
     </footer>
   );
 };
